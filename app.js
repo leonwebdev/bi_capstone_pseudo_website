2ae0ab06f15bccc83f26c4eeb8d749d241e55df4
Leon J <leonjohnson377@gmail.com>
2023-04-09 23:46:04 -0400

    event select input onchange done.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-month = document.getElementById("month");
-city = document.getElementById("city");
-img = document.getElementById("img");
-button = document.getElementById("button");
+let month = document.getElementById("month");
+let city = document.getElementById("city");
+// img = document.getElementById("img");
+let img = "";
+let img_box = document.getElementById("img_box");
+let button = document.getElementById("button");
 
 function changeImage() {
-    if (city.value && month.value) {
+    if (!city.value || !month.value) {
+        img_box.innerHTML = `
+        <h3 class="text-center pt-5">Please select month and city.</h3>
+        `;
+    } else {
+        city_value = city.value ? city.value : "v";
+        month_value = month.value ? month.value : "1";
+        img = "img/" + city_value + "_" + month_value + ".png";
+        img_box.innerHTML = `
+        <img src="${img}" class="img-fluid" />
+        `;
     }
-    city_value = city.value ? city.value : "v";
-    month_value = month.value ? month.value : "1";
-    img.src = "img/" + city_value + "_" + month_value + ".png";
 }
 
 button.addEventListener("click", changeImage);
+city.addEventListener("change", changeImage);
+month.addEventListener("change", changeImage);
